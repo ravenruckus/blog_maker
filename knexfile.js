523ea42357941388060257e5c5b54f923a96f158
ravenruckus <gyorihome@hotmail.com>
2017-01-14 13:28:43 -0800

    Close #1 create server.

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/bookshelf_dev'
+  },
+
+}
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+
+  }
---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config();
+// }
+
+const express = require('express');
+const app = express();
+
+app.disable('x-powered-by');
+
+const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+      app.use(morgan('short'));
+
+  default:
+}
+
+app.use(bodyParser.json());
+// app.use(cookieParser());
+
+const path = require('path');
+
+app.use(express.static(path.join('public')));
+
+// const posts = require('./routes/posts');
+
+// app.use(posts);
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+
+module.exports = app;
