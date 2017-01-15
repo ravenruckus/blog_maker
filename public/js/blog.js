9010edcdbd7ff4f206524ae6b9a0fc27a9650d32
ravenruckus <gyorihome@hotmail.com>
2017-01-15 14:22:47 -0800

    Close #4 Create blog.js

--- public/js/blog.js
+++ public/js/blog.js
@@ -3,5 +3,40 @@
 
     $('.parallax').parallax();
 
+    $.getJSON('/posts')
+      .done((posts) => {
+        const $posts = $('#posts');
+
+        for (const post of posts) {
+          const $anchor = $('<a>')
+            .attr({
+              href: `/blog.html?id=${post.id}`,
+              'data-delay': '50',
+              'data-tooltip': post.title
+            })
+            .tooltip();
+
+            const $col = $('<div>'.addClass('col s12 m6'));
+            const $card = $('<div>').addClass('card medium');
+            const $cardImage = $('<div>').addClass('card-image');
+            const $img = $('<img>').attr({ src: post.imgUrl, alt: post.title });
+            const $cardContent = $('<div>').addClass('card-content');
+            const $cardAction = $('<div>').addClass('card-action');
+
+            const $actionSection = $anchor.text(`${post.title} - ${post.created_at}`).appendTo($cardAction);
+            const $content = $('<p>').text(`${post.content}`).appendTo($cardContent);
+            const $imgSection = $img.appendTo($cardImage).appendTo($anchor);
+
+            $col.append($imgSection);
+            $col.append($content);
+            $col.append($actionSection);
+            $col.appendTo($posts);
+
+        }
+    })
+    .fail(() => {
+      Materialize.toast('Unable to retrieve posts', 3000);
+    })
+
 
 })();
