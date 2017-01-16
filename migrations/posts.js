5487b5431131b7b0df00e4aaabda3418058fa971
Amanda Charles <ancharles11@gmail.com>
2017-01-15 16:38:52 -0800

    Close #8 Create posts migration

---
+++ migrations/posts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('posts', (table) => {
+    table.increments();
+    table.integer('user_id');
+    table.text('content').notNullable().defaultTo('');
+    table.text('img').notNullable().defaultTo('');
+    table.string('title').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
+};
+exports.down = function(knex) {
+  return knex.schema.dropTable('posts');
+};
