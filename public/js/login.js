1405498a49106483533739e16c7d535ebd78a443
Amanda Charles <ancharles11@gmail.com>
2017-01-17 15:24:27 -0800

    created user route

---
+++ public/js/login.js
@@ -0,0 +1,34 @@
+(function() {
+  'use strict';
+
+  $('loginForm').submit((event) => {
+    event.preventDefault();
+
+    const email = $('#email').val().trim();
+    const password = $('#password').val()
+
+    if (!email) {
+      return Materialize.toast('Please provide an Email', 3500);
+    }
+
+    if (!password) {
+      return Materialize.toast('Please provide a Password', 3500);
+    }
+
+    const options = {
+      contentType: 'application/json',
+      data: JSON.stringify({ email, password}),
+      dataType: 'json',
+      type: 'POST',
+      url: '/token'
+    };
+
+    $.ajax(options)
+      .done(() => {
+        window.location.href = '/admin.html';
+      })
+      .fail(($xhr) => {
+        Materialize.toast($xhr.responseText, 3000);
+      });
+  });
+})();
---
+++ routes/users.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('../knex');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+
+const router = express.Router();
+
+router.post('/users', (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !email.trim()) {
+    return next(boom.create(400, 'Email must not be blank'));
+  }
+
+  if (!password || password.length < 8) {
+    return next(boom.create(
+      400,
+      'Password must be at least 8 characters long'
+    ));
+  }
+
+  knex('users')
+    .where('email', email)
+    .first()
+    .then((user) => {
+      if (user) {
+        throw boom.create(400, 'Email already exists');
+      }
+
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashedPassword) => {
+      const { firstName, lastName } = req.body;
+      const insertUser = { email, hashedPassword };
+
+      return knex('users').insert(decamelizeKeys(insertUser), '*');
+    })
+    .then((rows) => {
+      const user = camelizeKeys(rows[0]);
+      const claim = { userId: user.id };
+      const token = jwt.sign(claim, process.env.JWT_KEY, {
+        expiresIn: '30 days'
+      });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+        secure: router.get('env') === 'production'
+      });
+
+      delete user.hashedPassword;
+
+      res.send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
