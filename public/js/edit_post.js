8e068f9e203cafdb21c4bf2fe87b470bb579827c
ravenruckus <gyorihome@hotmail.com>
2017-01-17 13:09:50 -0800

    Close #19 make edit posts page

--- public/js/edit_post.js
+++ public/js/edit_post.js
@@ -1,6 +1,7 @@
 (function() {
   'use strict';
   const postId = window.QUERY_PARAMETERS.id;
+  console.log(postId);
 
   if (!postId) {
     window.location.href = '/all_posts.html';
@@ -11,25 +12,53 @@
     $('#img').val(post.img);
     $('#content').val(post.content);
 
-    Materialize.updateTextFields();
   };
 
   $.getJSON(`/posts/${postId}`)
     .done((post) => {
       renderPost(post);
-      console.log(post);
-      // const $post = $('#post');
-      // const $col = $('<div>').addClass('col s12 ind_post');
-      // const $title = $('<h2>').text(post.title);
-      // const $img = $('<img>').attr({ src: post.img, alt: post.title });
-      // const $p = $('<p>').text(post.content);
-      //
-      // $col.append($title);
-      // $col.append($img);
-      // $col.append($p);
-      // $post.append($col);
     })
     .fail(() => {
       Materialize.toast('Unable to retrieve post', 3000);
     });
-})();
+
+    $('#updatePost').click((event) => {
+      console.log(postId);
+      event.preventDefault();
+
+      const title = $('#title').val().trim();
+      const img = $('#img').val().trim();
+      const content = $('#content').val().trim();
+
+      if(!title) {
+        return Materialize.toast('Title must not be blank', 3000);
+      }
+
+      if(!img) {
+        return Materialize.toast('Please provide an image URL', 3000);
+      }
+
+      if(!content) {
+        return Materialize.toast('Please provide some content', 3000);
+      }
+
+      const options = {
+        contentType: 'application/json',
+        data: JSON.stringify({ title, img, content}),
+        dataType: 'json',
+        type: 'PATCH',
+        url: `posts/${postId}`
+      };
+      console.log(options);
+
+      $.ajax(options)
+        .done(() => {
+          console.log(postId);
+          window.location.href = `/post.html?id=${postId}`;
+        })
+        .fail(($xhr) => {
+          Materialize.toast($xhr.responseText, 3000)
+        });
+      });
+
+    })();
