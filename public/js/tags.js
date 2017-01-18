d4910cd4bf6f85fc0b36bb402f1c043900928e5c
ravenruckus <gyorihome@hotmail.com>
2017-01-18 10:50:17 -0800

    Close #36 Add event listener to tags and create array of marked items.

--- public/js/tags.js
+++ public/js/tags.js
@@ -1,30 +1,51 @@
 (function() {
     'use strict';
 
-    const attachListener = function(tag) {
-      $('#tagsForm').submit((event) => {
-        event.preventDefault();
+    // const attachListener = function(tag) {
+    //   $('#tagsForm').submit((event) => {
+    //     event.preventDefault();
+    //
+    //     const options = {
+    //       dataType: 'json',
+    //       type: 'DELETE',
+    //       url: `/tags/${tag.id}`
+    //     };
+    //
+    //     $.ajax(options)
+    //       .done(() => {
+    //         window.location.href = '/tags.html';
+    //       })
+    //       .fail(() => {
+    //         Materialize.toast('Unable to delete', 3000);
+    //       });
+    //     });
+    // };
+    // const attachListener = function(tag) {
+    //   $('#deleteTag').click((event) => {
+    //     event.preventDefault();
+    //
+    //     const options = {
+    //       dataType: 'json',
+    //       type: 'DELETE',
+    //       url: `/tags/${tags.id}`
+    //     };
+    //
+    //     $.ajax(options)
+    //       .done(() => {
+    //         window.location.href = '/tags.html';
+    //       })
+    //       .fail(() => {
+    //         Materialize.toast('Unable to delete', 3000);
+    //       });
+    //     });
+    // };
 
-        const options = {
-          dataType: 'json',
-          type: 'DELETE',
-          url: `/tags/${tag.id}`
-        };
 
-        $.ajax(options)
-          .done(() => {
-            window.location.href = '/tags.html';
-          })
-          .fail(() => {
-            Materialize.toast('Unable to delete', 3000);
-          });
-        });
-    };
 
 
     $.getJSON('/tags')
       .done((tags) => {
-        attachListener(tag);
+        // attachListener(tag);
 
         const $tagsForm = $('#tagsForm');
 
@@ -38,11 +59,21 @@
             $tagsForm.append($p);
 
        }
-       const $button = $('<a>').addClass('waves-effect waves-light btn-large').text('Delete').attr('id', 'deleteButton');
-       $tagsForm.append($button);
-
+      //  const $button = $('<a>').addClass('waves-effect waves-light btn-large').text('Delete').attr('id', 'deleteButton');
+      //  $tagsForm.append($button);
+      //  attachListener(tags);
     })
     .fail(() => {
       Materialize.toast('Unable to retrieve tags', 3000);
+    });
+    const delTags = [];
+    $('#tagsForm').on('click', 'input', (event) => {
+      console.log(event.target.id);
+      delTags.push(event.target.id);
     })
+
+    $('#deleteTag').click((event) => {
+      console.log(delTags);
+    });
+
 })();
