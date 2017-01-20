229f81fb40ecf5e25c831356e7514846a58deae9
ravenruckus <gyorihome@hotmail.com>
2017-01-19 16:40:34 -0800

    Close #60 Deploy to Heroku

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(() => {
       return knex('users').insert([{
   id: 1,
-  email: 'amy@email.com',
-  hashed_password: '$2a$12$bY9b7sA0.SikKPtH3Xg4JeJyjLpIuoVJAfpniWxEtU7sKvZpUfQei',
+  email: 'amy4@email.com',
+  hashed_password: '$2a$12$Hlqg26uuuV4xCHD25ubgHOHpbRvQyfeB/BniyYFLsjDZzzv/UXKDy',
   created_at: new Date('2016-06-06 14:26:16 UTC'),
   updated_at: new Date('2016-07-07 14:26:16 UTC')
     }])
