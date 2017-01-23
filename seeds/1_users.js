9a135fc768042bbb133222ad11d7db39287487e6
ravenruckus <gyorihome@hotmail.com>
2017-01-23 07:28:40 -0800

    Fix seed error

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       return knex('users').insert([{
   id: 1,
   email: 'amy8@email.com',
-  hashed_password: 'password',
+  hashed_password: '$2a$12$4eZ3DS2Ww1s3A5q7ioQeteDOvvC6IXAYLh7CZrm4h6FjeeYcHF2va',
   created_at: new Date('2016-06-06 14:26:16 UTC'),
   updated_at: new Date('2016-07-07 14:26:16 UTC')
     }])
